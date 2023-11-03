3e874ad87dca3e0f5c984697e659763488ddddc7
Javier Martinez <fjmartinez@disitec.es>
2023-11-03 13:40:40 +0100

    add toggle

--- src/lib/InputToggle.svelte
+++ src/lib/InputToggle.svelte
@@ -1,21 +1,31 @@
 <script lang="ts">
+  import clsx from 'clsx'
+  import { createEventDispatcher } from 'svelte'
   import { createSwitch } from 'svelte-headlessui'
+  import { dispatchWcEvent } from './wcdispatch.js'
 
+  const dispatch = createEventDispatcher()
   const sw = createSwitch({ label: 'Set Preference' })
+
+  let rootEl: HTMLElement
 
   export let checked = false
 
-  $: {
-    $sw.checked = checked
+  $: $sw.checked = checked
+
+  $: togleStyles = clsx({ 'bg-gray-200': !$sw.checked }, { 'bg-accent-500': $sw.checked })
+
+  function handleChange() {
+    dispatch('change', $sw.checked)
+    dispatchWcEvent(rootEl, 'change', $sw.checked)
   }
 </script>
 
-<div class="py-16 flex w-full flex-col items-center justify-center">
+<div bind:this={rootEl} class="flex w-full flex-col items-center justify-center">
   <button
-    class:bg-gray-200={!$sw.checked}
-    class:bg-accent-500={$sw.checked}
-    class="relative inline-flex h-5 w-8 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-0 focus:ring-offset-0"
+    class=" {togleStyles} relative inline-flex h-5 w-8 flex-shrink-0 cursor-pointer rounded-full border-2 border-transparent transition-colors duration-200 ease-in-out focus:outline-none focus:ring-0 focus:ring-offset-0"
     use:sw.toggle
+    on:click={handleChange}
   >
     <span class="sr-only">Use setting</span>
     <span
---
+++ src/web-components/toggle.ts
@@ -0,0 +1,20 @@
+import InputToggle from '../lib/InputToggle.svelte'
+import WebComponent from './component.js'
+
+export default class extends WebComponent {
+  constructor() {
+    super()
+
+    this._element = new InputToggle({
+      // Tell it that it lives in the shadow root
+      target: this._shadowRoot,
+      // Pass any props
+      props: this.createProps({
+        // This is the place where you do any conversion between
+        // the native string attributes and the types you expect
+        // in your svelte components
+        checked: this.getAttribute('checked') === 'true' || false
+      })
+    })
+  }
+}

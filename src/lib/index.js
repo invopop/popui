ac5a0bcaf53060d32191dd7b8d6274e037f1651d
Javier Martinez <javier.martinez@invopop.com>
2023-11-08 13:07:32 +0000

    add twind theme as exportable file

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,61 @@
 import BaseButton from './BaseButton.svelte'
+import BaseCounter from './BaseCounter.svelte'
+import BaseDropdown from './BaseDropdown.svelte'
+import ButtonFile from './ButtonFile.svelte'
+import CompanySelector from './CompanySelector.svelte'
+import DataListItem from './DataListItem.svelte'
+import DatePicker from './DatePicker.svelte'
+import DrawerContext from './DrawerContext.svelte'
+import FeedEvents from './FeedEvents.svelte'
+import FeedIconEvent from './FeedIconEvent.svelte'
+import FeedIconStatus from './FeedIconStatus.svelte'
+import FeedItem from './FeedItem.svelte'
+import FeedViewer from './FeedViewer.svelte'
+import InputCheckbox from './InputCheckbox.svelte'
+import InputLabel from './InputLabel.svelte'
+import InputRadio from './InputRadio.svelte'
+import InputSearch from './InputSearch.svelte'
+import InputSelect from './InputSelect.svelte'
+import InputText from './InputText.svelte'
+import InputToggle from './InputToggle.svelte'
+import MenuItem from './MenuItem.svelte'
+import ProfileAvatar from './ProfileAvatar.svelte'
+import SectionLayout from './SectionLayout.svelte'
+import Tabs from './Tabs.svelte'
+import TagSearch from './TagSearch.svelte'
+import TagStatus from './TagStatus.svelte'
+import TitleMain from './TitleMain.svelte'
+import TitleSection from './TitleSection.svelte'
+import UuidCopy from './UuidCopy.svelte'
 
-export { BaseButton }
+export {
+  BaseButton,
+  BaseCounter,
+  BaseDropdown,
+  ButtonFile,
+  CompanySelector,
+  DataListItem,
+  DatePicker,
+  DrawerContext,
+  FeedEvents,
+  FeedIconEvent,
+  FeedIconStatus,
+  FeedItem,
+  FeedViewer,
+  InputCheckbox,
+  InputLabel,
+  InputRadio,
+  InputSearch,
+  InputSelect,
+  InputText,
+  InputToggle,
+  MenuItem,
+  ProfileAvatar,
+  SectionLayout,
+  Tabs,
+  TagSearch,
+  TagStatus,
+  TitleMain,
+  TitleSection,
+  UuidCopy
+}

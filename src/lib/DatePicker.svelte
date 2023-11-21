ca75cc85287f1fefed629122e4acae7b2511dee4
Javier Martinez <fjmartinez@disitec.es>
2023-11-21 17:23:32 +0100

    use flatpicker for Datepicker

--- src/lib/DatePicker.svelte
+++ src/lib/DatePicker.svelte
@@ -1,42 +1,74 @@
 <script lang="ts">
+  import Transition from 'svelte-transition'
   import flatpickr from 'flatpickr'
+  import type { Instance } from 'flatpickr/dist/types/instance.js'
   import 'flatpickr/dist/flatpickr.min.css'
   import { createEventDispatcher, onMount } from 'svelte'
+  import BaseButton from './BaseButton.svelte'
+  import {
+    startOfWeek,
+    endOfWeek,
+    subWeeks,
+    endOfMonth,
+    startOfMonth,
+    subMonths,
+    startOfQuarter,
+    endOfQuarter,
+    subQuarters
+  } from 'date-fns'
 
   const dispatch = createEventDispatcher()
 
   export let label = 'Date'
+  export let position: 'left' | 'right' = 'left'
 
   let datePickerEl: HTMLElement
-  let selectedLabel = ''
+  let selectedLabel = label
+  let datepicker: Instance
+  let selectedDates = { from: '', to: '' }
+  let isOpen = false
+
+  const today = new Date()
+  const startOfThisWeek = startOfWeek(today, { weekStartsOn: 1 })
+  const endOfThisWeek = endOfWeek(today, { weekStartsOn: 1 })
+  const startOfLastWeek = subWeeks(startOfThisWeek, 1)
+  const endOfLastWeek = subWeeks(endOfThisWeek, 1)
+  const startOfThisMonth = startOfMonth(today)
+  const endOfThisMonth = endOfMonth(today)
+  const startOfLastMonth = subMonths(startOfThisMonth, 1)
+  const endOfLastMonth = subMonths(endOfThisMonth, 1)
+  const startOfThisQuarter = startOfQuarter(today)
+  const endOfThisQuarter = endOfQuarter(today)
+  const startOfLastQuarter = subQuarters(startOfThisQuarter, 1)
+  const endOfLastQuarter = subQuarters(endOfThisQuarter, 1)
 
   onMount(() => {
-    flatpickr(datePickerEl, {
-      onChange: function (selectedDates: Date[], datesAsText: string) {
-        if (selectedDates.length === 0) {
-          clearDates()
+    datepicker = flatpickr(datePickerEl, {
+      onChange: function (dates: Date[]) {
+        if (dates.length === 0) {
+          selectedLabel = label
           return
         }
 
-        if (selectedDates.length === 1) return
+        if (dates.length === 1) return
 
-        const from = getDate(selectedDates[0])
-        const to = getDate(selectedDates[1])
+        const from = getDate(dates[0])
+        const to = getDate(dates[1])
 
-        selectedLabel = datesAsText
-
-        dispatch('selected', { from, to })
+        selectedDates = { from, to }
       },
-      wrap: true,
+      locale: {
+        firstDayOfWeek: 1,
+        weekdays: {
+          shorthand: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+          longhand: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+        }
+      },
+      inline: true,
       mode: 'range',
-      // minDate: "today",
-      dateFormat: 'Y-m-d'
-      // disable: [
-      //     function(date) {
-      //         // disable every multiple of 8
-      //         return !(date.getDate() % 8);
-      //     }
-      // ]
+      dateFormat: 'Y-m-d',
+      showMonths: 2,
+      numberOfMonths: 2
     })
   })
 
@@ -48,19 +80,65 @@
     return `${year}-${month}-${day}`
   }
 
+  function cancel() {
+    isOpen = false
+    datepicker.clear()
+  }
+
+  function confirm() {
+    isOpen = false
+    selectedLabel = selectedDates.from ? `${selectedDates.from} → ${selectedDates.to}` : label
+    dispatch('selected', selectedDates)
+  }
+
   function clearDates() {
-    selectedLabel = ''
-    dispatch('selected', { from: '', to: '' })
+    datepicker.clear()
+    selectedDates = { from: '', to: '' }
+    confirm()
+  }
+
+  function onThisWeek() {
+    datepicker.setDate([startOfThisWeek, endOfThisWeek], true)
+    datepicker.jumpToDate(startOfThisWeek)
+  }
+
+  function onLastWeek() {
+    datepicker.setDate([startOfLastWeek, endOfLastWeek], true)
+    datepicker.jumpToDate(startOfLastWeek)
+  }
+
+  function onThisMonth() {
+    datepicker.setDate([startOfThisMonth, endOfThisMonth], true)
+    datepicker.jumpToDate(startOfThisMonth)
+  }
+
+  function onLastMonth() {
+    datepicker.setDate([startOfLastMonth, endOfLastMonth], true)
+    datepicker.jumpToDate(startOfLastMonth)
+  }
+
+  function onThisQuarter() {
+    datepicker.setDate([startOfThisQuarter, endOfThisQuarter], true)
+    datepicker.jumpToDate(startOfThisQuarter)
+  }
+
+  function onLastQuarter() {
+    datepicker.setDate([startOfLastQuarter, endOfLastQuarter], true)
+    datepicker.jumpToDate(startOfLastQuarter)
   }
 </script>
 
-<div bind:this={datePickerEl} class="relative">
-  <input
-    placeholder={label}
-    class="datepicker-trigger py-1.25 pl-3 border border-neutral-200 hover:border-neutral-300 w-full rounded text-neutral-800 placeholder-neutral-800 text-base outline-accent-400"
-    data-input
-  />
-  <button data-clear class:hidden={!selectedLabel} class="absolute right-10 top-2 mt-px z-20">
+<div class="relative">
+  <button
+    on:click={() => (isOpen = !isOpen)}
+    class="datepicker-trigger w-full py-1.25 pl-3 border border-neutral-200 hover:border-neutral-300 rounded text-neutral-800 placeholder-neutral-800 text-base outline-accent-400"
+    >{selectedLabel}</button
+  >
+  <button
+    on:click={clearDates}
+    class:hidden={selectedLabel === label}
+    class="absolute right-10 top-2 mt-px z-20"
+  >
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
@@ -78,7 +156,244 @@
   </button>
 </div>
 
+<div class="relative">
+  <Transition
+    show={isOpen}
+    enter="transition ease-out duration-100"
+    enterFrom="transform opacity-0 scale-95"
+    enterTo="transform opacity-100 scale-100"
+    leave="transition ease-in duration-75"
+    leaveFrom="transform opacity-100 scale-100"
+    leaveTo="transform opacity-0 scale-95"
+  >
+    <div
+      class:left-0={position === 'left'}
+      class:right-0={position === 'right'}
+      class="inline-flex flex-col shadow rounded-lg absolute right-0 top-2"
+    >
+      <div class="flex border-b border-neutral-100">
+        <div class="flex flex-col justify-around items-start px-3 py-2 border-r border-neutral-100">
+          <button
+            on:click={onThisWeek}
+            class="whitespace-nowrap text-neutral-500 text-sm px-2 py-1.5">This Week</button
+          >
+          <button
+            on:click={onLastWeek}
+            class="whitespace-nowrap text-neutral-500 text-sm px-2 py-1.5">Last Week</button
+          >
+          <button
+            on:click={onThisMonth}
+            class="whitespace-nowrap text-neutral-500 text-sm px-2 py-1.5">This Month</button
+          >
+          <button
+            on:click={onLastMonth}
+            class="whitespace-nowrap text-neutral-500 text-sm px-2 py-1.5">Last Month</button
+          >
+          <button
+            on:click={onThisQuarter}
+            class="whitespace-nowrap text-neutral-500 text-sm px-2 py-1.5">This Quarter</button
+          >
+          <button
+            on:click={onLastQuarter}
+            class="whitespace-nowrap text-neutral-500 text-sm px-2 py-1.5">Last Quarter</button
+          >
+        </div>
+        <div bind:this={datePickerEl} />
+      </div>
+      <div class="p-4 flex justify-end items-center space-x-3">
+        <BaseButton variant="secondary" on:click={cancel}>Cancel</BaseButton>
+        <BaseButton variant="primary" on:click={confirm} disabled={!selectedDates.to}
+          >Confirm</BaseButton
+        >
+      </div>
+    </div>
+  </Transition>
+</div>
+
 <style>
+  :global(.flatpickr-calendar) {
+    width: 508px !important;
+    padding-left: 12px;
+    padding-right: 12px;
+    padding-top: 4px;
+    padding-bottom: 8px;
+    font-family: 'Inter' !important;
+    top: 0 !important;
+    border-radius: 0;
+    box-shadow: none;
+  }
+  :global(.flatpickr-weekdaycontainer:nth-child(2)) {
+    margin-left: 28px;
+  }
+  :global(.dayContainer:nth-child(1)) {
+    margin-right: 28px;
+  }
+  :global(.flatpickr-months) {
+    padding-top: 8px;
+    padding-bottom: 12px;
+  }
+  :global(.flatpickr-current-month) {
+    padding-top: 0px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 15px;
+    height: 24px !important;
+  }
+  :global(.flatpickr-monthDropdown-months) {
+    font-weight: 500 !important;
+    height: 22px !important;
+    color: #09101c !important;
+  }
+  :global(.cur-month:hover) {
+    background-color: transparent !important;
+  }
+  :global(.numInputWrapper:hover) {
+    background-color: #fafbfb !important;
+    border: 1px solid #f3f5f5 !important;
+  }
+  :global(.numInputWrapper span.arrowUp) {
+    right: -4px;
+    border: 0;
+    background-color: transparent;
+  }
+  :global(.numInputWrapper span.arrowDown) {
+    right: -4px;
+    border: 0;
+    background-color: transparent;
+  }
+  :global(.cur-year, .cur-month) {
+    font-weight: 600 !important;
+    color: #09101c !important;
+  }
+  :global(.numInputWrapper) {
+    display: flex !important;
+    align-items: center;
+    justify-content: center;
+    height: 22px;
+  }
+  :global(.flatpickr-next-month) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 24px !important;
+    width: 24px !important;
+    border: 1px solid #e9ebeb;
+    right: 12px !important;
+    top: 12px !important;
+    padding: 0 !important;
+    border-radius: 4px;
+  }
+  :global(.flatpickr-prev-month) {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 24px !important;
+    width: 24px !important;
+    border: 1px solid #e9ebeb;
+    left: 12px !important;
+    top: 12px !important;
+    padding: 0 !important;
+    border-radius: 4px;
+  }
+  :global(.flatpickr-weekdays) {
+    height: 32px;
+  }
+  :global(.flatpickr-day.selected, .flatpickr-day.startRange, .flatpickr-day.endRange) {
+    background-color: #169958 !important;
+    border: 0;
+    border-radius: 4px !important;
+    box-shadow: none !important;
+  }
+  :global(
+      .flatpickr-day.selected:hover,
+      .flatpickr-day.startRange:hover,
+      .flatpickr-day.endRange:hover
+    ) {
+    border: 0;
+  }
+  :global(.flatpickr-day.inRange) {
+    background-color: #f4f5f5;
+    border: 1px solid #f4f5f5;
+    box-shadow: none !important;
+  }
+  :global(.prevMonthDay, .nextMonthDay) {
+    color: #cccece !important;
+    font-weight: 500;
+    font-size: 13px;
+  }
+  :global(.prevMonthDay.inRange, .nextMonthDay.inRange) {
+    background-color: #f4f5f5 !important;
+    border: 1px solid #f4f5f5 !important;
+  }
+  :global(.flatpickr-weekday) {
+    width: 32px;
+    height: 32px;
+    display: flex !important;
+    align-items: center;
+    justify-content: center;
+    font-weight: 500 !important;
+    color: #7e7f7f !important;
+    font-size: 13px !important;
+  }
+  :global(.flatpickr-weekdaycontainer) {
+    flex: 0 !important;
+    border-bottom: 1px solid #f3f5f5 !important;
+  }
+  :global(.flatpickr-prev-month svg) {
+    fill: #0a0a0a;
+    height: 10px !important;
+    width: 10px !important;
+  }
+  :global(.flatpickr-next-month svg) {
+    fill: #0a0a0a;
+    height: 10px !important;
+    width: 10px !important;
+  }
+  :global(.flatpickr-prev-month:hover svg, .flatpickr-next-month:hover svg) {
+    fill: #0a0a0a !important;
+  }
+  :global(.flatpickr-days) {
+    width: 496px !important;
+  }
+  :global(.dayContainer) {
+    width: 224px;
+    min-width: 224px;
+    max-width: 224px;
+    justify-content: start;
+    box-shadow: none !important;
+  }
+  :global(.flatpickr-day) {
+    margin-top: 2px !important;
+    border-radius: 4px;
+    width: 32px;
+    max-width: 32px;
+    height: 32px;
+    line-height: 32px;
+    display: flex !important;
+    align-items: center;
+    justify-content: center;
+    color: #0a0a0a;
+  }
+  :global(.flatpickr-day.today) {
+    border: 0;
+    border-bottom: 1px solid #169958;
+    border-radius: 0;
+  }
+  :global(.flatpickr-day.today:hover) {
+    background-color: #fafbfb !important;
+    border: 1px solid #f3f5f5 !important;
+    color: #0a0a0a;
+    border-radius: 4px;
+  }
+  :global(.flatpickr-day:hover) {
+    background-color: #fafbfb;
+    border: 1px solid #f3f5f5;
+  }
+  :global(.prevMonthDay:hover, .nextMonthDay:hover) {
+    background-color: #fafbfb !important;
+    border: 1px solid #f3f5f5 !important;
+  }
   .datepicker-trigger {
     appearance: none;
     -webkit-appearance: none;
--- src/stories/DatePicker.stories.ts
+++ src/stories/DatePicker.stories.ts
@@ -2,22 +2,28 @@ import type { Meta, StoryObj } from '@storybook/svelte'
 
 import DatePicker from '../lib/DatePicker.svelte'
 import MarginDecorator from './decorartors/MarginDecorator.svelte'
-import MaxWidthSmDecorator from './decorartors/MaxWidthSmDecorator.svelte'
 
 // More on how to set up stories at: https://storybook.js.org/docs/svelte/writing-stories/introduction
 const meta = {
   title: 'Components/DatePicker',
   component: DatePicker,
   tags: ['autodocs'],
-  decorators: [() => MarginDecorator, () => MaxWidthSmDecorator]
+  decorators: [() => MarginDecorator]
 } satisfies Meta<DatePicker>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
 // More on writing stories with args: https://storybook.js.org/docs/svelte/writing-stories/args
-export const NoSelectedDate: Story = {
+export const LeftPosition: Story = {
   args: {
     label: 'Pick a Date'
+  }
+}
+
+export const RightPosition: Story = {
+  args: {
+    label: 'Pick a Date',
+    position: 'right'
   }
 }

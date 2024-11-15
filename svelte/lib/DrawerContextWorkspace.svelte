9a0de8eb5045eda80f4d2a7e875bc36575a42037
Javier Martinez <javier.martinez@invopop.com>
2024-11-15 12:35:31 +0000

    workspace selector animations

--- svelte/lib/DrawerContextWorkspace.svelte
+++ svelte/lib/DrawerContextWorkspace.svelte
@@ -2,10 +2,12 @@
   import type { DrawerOption } from './types.ts'
   import DrawerContextItem from './DrawerContextItem.svelte'
   import { Icon } from '@steeze-ui/svelte-icon'
-  import { AddCircle, ChevronDown, ChevronRight, Settings, Sidebar } from '@invopop/ui-icons'
+  import { AddCircle, Settings, Sidebar } from '@invopop/ui-icons'
   import { createEventDispatcher } from 'svelte'
   import BaseCounter from './BaseCounter.svelte'
   import EmptyStateIcon from './EmptyStateIcon.svelte'
+  import { slide } from 'svelte/transition'
+  import { ChevronRight } from '@steeze-ui/heroicons'
 
   export let items: DrawerOption[] = []
   export let multiple = false
@@ -28,16 +30,21 @@
   }
 </script>
 
-<div class="w-[300px] border border-neutral-200 rounded-sm shadow-lg bg-white">
+<div class="w-[300px] border border-neutral-200 rounded-md shadow-lg bg-white">
   <div class="max-h-[60vh] overflow-y-auto rounded-sm">
     <button
-      class="flex items-center justify-between bg-neutral-50 border-b border-neutral-200 rounded-t-sm h-8 py-1.5 pl-1.5 pr-2 text-base font-medium text-neutral-800 w-full"
+      class="flex items-center justify-between bg-neutral-50 border-b border-neutral-200 rounded-t-sm h-9 py-2 pl-2.5 pr-3 text-base font-medium text-neutral-800 w-full"
       on:click={() => {
-        liveOpen = !liveOpen
+        if (liveOpen) return
+        liveOpen = true
+        sandboxOpen = false
       }}
     >
       <div class="flex items-center space-x-1.5">
-        <Icon src={liveOpen ? ChevronDown : ChevronRight} class="h-4 w-4 text-neutral-800" />
+        <Icon
+          src={ChevronRight}
+          class="h-4 w-4 text-neutral-500 transition-all transform {liveOpen ? 'rotate-90' : ''}"
+        />
         <span>Live</span>
       </div>
       {#if liveItems.length}
@@ -45,30 +52,37 @@
       {/if}
     </button>
     {#if liveOpen}
-      {#if !liveItems.length}
-        <div class="h-[182px]">
-          <EmptyStateIcon
-            icon={Sidebar}
-            title="No workspaces here"
-            description="Create a workspace to start"
-          />
-        </div>
-      {/if}
-      <ul class="p-1 pb-0.5 space-y-1">
-        {#each liveItems as item}
-          <DrawerContextItem {item} {multiple} workspace on:click />
-        {/each}
-      </ul>
+      <div transition:slide>
+        {#if !liveItems.length}
+          <div class="h-[182px]">
+            <EmptyStateIcon
+              icon={Sidebar}
+              title="No workspaces here"
+              description="Create a workspace to start"
+            />
+          </div>
+        {/if}
+        <ul class="p-1 pb-0.5 space-y-1">
+          {#each liveItems as item}
+            <DrawerContextItem {item} {multiple} workspace on:click />
+          {/each}
+        </ul>
+      </div>
     {/if}
     <button
-      class="flex items-center justify-between bg-neutral-50 border-b border-neutral-200 h-8 py-1.5 pl-1.5 pr-2 text-base font-medium text-neutral-800 w-full"
+      class="flex items-center justify-between bg-neutral-50 border-b border-neutral-200 h-9 py-2 pl-2.5 pr-3 text-base font-medium text-neutral-800 w-full"
       class:border-t={liveOpen}
       on:click={() => {
-        sandboxOpen = !sandboxOpen
+        if (sandboxOpen) return
+        sandboxOpen = true
+        liveOpen = false
       }}
     >
       <div class="flex items-center space-x-1.5">
-        <Icon src={sandboxOpen ? ChevronDown : ChevronRight} class="h-4 w-4 text-neutral-800" />
+        <Icon
+          src={ChevronRight}
+          class="h-4 w-4 text-neutral-500 transition-all transform {sandboxOpen ? 'rotate-90' : ''}"
+        />
         <span>Sandbox</span>
       </div>
       {#if sandboxItems.length}
@@ -76,20 +90,22 @@
       {/if}
     </button>
     {#if sandboxOpen}
-      {#if !sandboxItems.length}
-        <div class="h-[182px]">
-          <EmptyStateIcon
-            icon={Sidebar}
-            title="No workspaces here"
-            description="Create a workspace to start"
-          />
-        </div>
-      {/if}
-      <ul class="p-1 pb-0.5 space-y-1">
-        {#each sandboxItems as item}
-          <DrawerContextItem {item} {multiple} workspace on:click />
-        {/each}
-      </ul>
+      <div transition:slide>
+        {#if !sandboxItems.length}
+          <div class="h-[182px]">
+            <EmptyStateIcon
+              icon={Sidebar}
+              title="No workspaces here"
+              description="Create a workspace to start"
+            />
+          </div>
+        {/if}
+        <ul class="p-1 pb-0.5 space-y-1">
+          {#each sandboxItems as item}
+            <DrawerContextItem {item} {multiple} workspace on:click />
+          {/each}
+        </ul>
+      </div>
     {/if}
   </div>
 
@@ -102,7 +118,7 @@
         }}
       >
         <Icon src={AddCircle} class="w-4 h-4 text-neutral-500 flex-shrink-0" />
-        <span class="text-neutral-800 tracking-tight text-base">Create workspace</span>
+        <span class="text-neutral-800 tracking-tight text-base font-medium">Create workspace</span>
       </button>
     </li>
     <li class="pl-1.5 py-1.5 pr-2 hover:bg-neutral-100 rounded-sm">
@@ -113,7 +129,7 @@
         }}
       >
         <Icon src={Settings} class="w-4 h-4 text-neutral-500" />
-        <span class="text-neutral-800 tracking-tight text-base">Settings</span>
+        <span class="text-neutral-800 tracking-tight text-base font-medium">Settings</span>
       </button>
     </li>
   </ul>

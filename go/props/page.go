9819179ed6c232554c92a94ecc5f5c4a1c2b3f27
Javier Martinez <javier.martinez@invopop.com>
2025-01-08 10:16:58 +0000

    fix comment

--- go/props/page.go
+++ go/props/page.go
@@ -10,7 +10,8 @@ type PageHeader struct {
 	Breadcrumbs []PageBreadcrumb
 }
 
-// PageBreadcrumb defines a Page Breadcrumb to be displayed on a PageHeader
+// PageBreadcrumb defines a Page Breadcrumb to be displayed on a PageHeader.
+// Attrs can be used to add extra properties such as target="_blank"
 type PageBreadcrumb struct {
 	Name  string
 	URL   templ.SafeURL

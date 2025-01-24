a065783e508f020cd1e700afef5de9b5d315af4e
Javier Martinez <javier.martinez@invopop.com>
2025-01-24 14:49:55 +0000

    add preview html file on public path

--- go/cmd/popui/serve.go
+++ go/cmd/popui/serve.go
@@ -3,7 +3,11 @@ package main
 import (
 	"context"
 	"errors"
+	"io"
+	"log"
 	"net/http"
+	"os"
+	"path"
 	"time"
 
 	"github.com/a-h/templ"
@@ -39,6 +43,7 @@ func (s *serveOpts) runE(cmd *cobra.Command, _ []string) error {
 	ctx, cancel := context.WithCancel(cmd.Context())
 	defer cancel()
 
+	// Boot up development server
 	e := echo.New()
 
 	e.StaticFS(popui.AssetPath, popui.Assets)
@@ -46,6 +51,39 @@ func (s *serveOpts) runE(cmd *cobra.Command, _ []string) error {
 	e.GET("/", s.index)
 	e.GET("/prose", s.prose)
 	e.GET("/admin", s.admin)
+
+	// Generate static html file for compiled examples
+	publicPath := "public"
+	err := createFolder(publicPath)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	// Create index.html file on public path
+	name := path.Join(publicPath, "index.html")
+	f, err := os.Create(name)
+	if err != nil {
+		log.Fatalf("failed to create output file: %v", err)
+	}
+	// Render Admin example templ component
+	err = examples.Admin().Render(context.Background(), f)
+	if err != nil {
+		log.Fatalf("failed to write index page: %v", err)
+	}
+	// Copy assets to public path
+	assetsPath := "go/assets"
+	publicAssetPath := publicPath + popui.AssetPath + "/assets"
+	err = createFolder(publicAssetPath)
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	err = copyFile(assetsPath+"/popui.css", publicAssetPath+"/popui.css")
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	err = copyFile(assetsPath+"/popui.js", publicAssetPath+"/popui.js")
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
 
 	var startErr error
 	go func() {
@@ -87,6 +125,39 @@ func render(c echo.Context, status int, t templ.Component) error { //nolint:unpa
 
 	if err := t.Render(c.Request().Context(), c.Response().Writer); err != nil {
 		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
+	}
+	return nil
+}
+
+// copyFile copies a file from source to destination path
+func copyFile(src, dst string) error {
+	source, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	destination, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+	defer destination.Close()
+
+	_, err = io.Copy(destination, source)
+	return err
+}
+
+// createFolder creates a folder if it does not exist
+func createFolder(folderPath string) error {
+	_, err := os.Stat(folderPath)
+	if os.IsNotExist(err) {
+		err := os.MkdirAll(folderPath, 0755)
+		if err != nil {
+			return err
+		}
+		log.Printf("Folder created: %s\n", folderPath)
+	} else if err != nil {
+		return err
 	}
 	return nil
 }

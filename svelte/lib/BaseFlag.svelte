e8ac3888c999abfba727979ecd2be220e2e5d66c
Javier Martinez <javier.martinez@invopop.com>
2025-08-19 14:10:26 +0000

    remove global option for BaseFlag

--- svelte/lib/BaseFlag.svelte
+++ svelte/lib/BaseFlag.svelte
@@ -1,16 +1,9 @@
 <script lang="ts">
-  import { World } from '@invopop/ui-icons'
-  import { Icon } from '@steeze-ui/svelte-icon'
-
   export let country = ''
   export let width = 12
 
   $: iso = country.toLowerCase()
 </script>
 
-{#if country === 'global'}
-  <Icon src={World} style={`width: ${width}px;`} />
-{:else}
-  <!-- https://github.com/HatScripts/circle-flags -->
-  <img src={`https://hatscripts.github.io/circle-flags/flags/${iso}.svg`} alt={country} {width} />
-{/if}
+<!-- https://github.com/HatScripts/circle-flags -->
+<img src={`https://hatscripts.github.io/circle-flags/flags/${iso}.svg`} alt={country} {width} />

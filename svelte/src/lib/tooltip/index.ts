6198c8a4150250806678a996567b8f8af35b6bdf
Javier Martinez <javier.martinez@invopop.com>
2026-04-07 12:59:38 +0000

    update bitsui types

--- svelte/src/lib/tooltip/index.ts
+++ svelte/src/lib/tooltip/index.ts
@@ -2,9 +2,9 @@ import { Tooltip as TooltipPrimitive } from 'bits-ui'
 import Trigger from './tooltip-trigger.svelte'
 import Content from './tooltip-content.svelte'
 import AutoHide from './tooltip-auto-hide.svelte'
-const Root = TooltipPrimitive.Root
-const Provider = TooltipPrimitive.Provider
-const Portal = TooltipPrimitive.Portal
+const Root: typeof TooltipPrimitive.Root = TooltipPrimitive.Root
+const Provider: typeof TooltipPrimitive.Provider = TooltipPrimitive.Provider
+const Portal: typeof TooltipPrimitive.Portal = TooltipPrimitive.Portal
 export {
   Root,
   Trigger,
